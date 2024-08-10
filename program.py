a6ea5dc642f8f1ab01ee6972e6570969147d44f8
Tooba Razzaq <125481023+ToobaHaya@users.noreply.github.com>
2024-08-10 13:09:50 +0500

    basics of python

---
+++ program.py
@@ -0,0 +1,70 @@
+# variables for strings
+
+first_name = "Tooba"
+Last_name = "Haya"
+print(first_name + " " + Last_name)
+
+# Variable for Numbers 
+
+num = 25
+new_num = num + 5
+print(new_num)
+
+# Maths Expression 
+
+add = 2+5
+print(add)
+
+sub = 7-3
+print(sub)
+
+multiply = 4*5
+print(multiply)
+
+divide = 9/4
+print(divide)
+
+square = 2**2
+print(square)
+
+remainder = 5%2
+print(remainder)
+
+age = 21
+age += 3
+print(age)
+
+age -= 2
+print(age)
+
+age *= 2
+print(age)
+
+# Concatenation
+
+print("The sum of 2+2 is " + "4")
+
+
+# if statements
+name = "haya"
+if name == "haya":
+    print("correct")
+
+# if else
+bottle_color = "black"
+if bottle_color == "pink":
+    print("target achieved")
+else:
+    print("Next Time")
+
+# elif
+buy_score = 1
+donut = input("How your donut look like ")
+if donut == "fresh":
+    buy_score += 5
+elif donut ==  "average":
+    buy_score += 2 
+else:
+    buy_score -=1
+print(buy_score)
+
